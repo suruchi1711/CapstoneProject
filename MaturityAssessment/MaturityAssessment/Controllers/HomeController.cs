de81c57da13b67f4d292dc950b245f8e14d41573
AnkitSenapati <102649490+AnkitSenapati@users.noreply.github.com>
2022-03-31 17:19:41 +0530

    Updated ActionMethod
    New Commit

--- MaturityAssessment/MaturityAssessment/Controllers/HomeController.cs
+++ MaturityAssessment/MaturityAssessment/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MaturityAssessment.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
